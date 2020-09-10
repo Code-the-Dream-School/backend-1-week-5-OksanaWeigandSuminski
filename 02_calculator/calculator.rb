d6bf89b56037d767a49957d8ad06258e7e6760d3
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2020-09-10 16:39:46 -0400

    completed assignment for backend I week 5

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add (var1, var2)
+    var1 + var2
+end
+
+def subtract (var1, var2)
+    var1 - var2
+end
+
+def sum(array)
+    sum = 0.0
+    if array.length > 0 then
+        array.each do |item|
+        sum += item
+        end
+    end
+    return sum
+end
